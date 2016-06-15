7ce873b7f9398c1fe1e12177f18adaf010838231
Richard Collins <richard@tawherotech.nz>
2016-06-15 13:04:11 +1200

    Add/remove hosts

--- src/hosts.go
+++ src/hosts.go
@@ -3,38 +3,73 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
-const hostsHelp = `Usage: infr hosts [-a address]`
+const hostsHelp = `Usage: infr hosts [options] [name]`
 
 const infrDomain = "infr.tawherotech.nz"
 
-var hostsAdd, hostsRemove string
+var hostsAdd, hostsRemove bool
 
 type host struct {
 	Name string
 }
 
 func hostsFlags(fs *flag.FlagSet) {
-	fs.StringVar(&hostsAdd, "a", "", "Add host to cluster")
-	fs.StringVar(&hostsRemove, "r", "", "Remove host from cluster")
+	fs.BoolVar(&hostsAdd, "a", false, "Add host 'name' to cluster")
+	fs.BoolVar(&hostsRemove, "r", false, "Remove host 'name' from cluster")
 }
 
 func hosts(args []string) {
+	var name string
+
+	switch len(args) {
+	case 0:
+		if hostsAdd || hostsRemove {
+			errorHelpAndExit("hosts", "That option requires 'name' to be specified.")
+		}
+	case 1:
+		name = args[0]
+		if hostsAdd && hostsRemove {
+			errorHelpAndExit("hosts", "You cannot add and remove a host at the same time.")
+		}
+		if !hostsAdd && !hostsRemove {
+			errorHelpAndExit("hosts", "Please specify an option so I know what to do with that host.")
+		}
+	default:
+		errorHelpAndExit("hosts", "Too many arguments.")
+	}
+
 	var hosts []host
 	loadConfig("hosts", &hosts)
 
-	if hostsAdd != "" {
-		hosts = append(hosts, host{hostsAdd})
+	if hostsAdd {
+		for _, host := range hosts {
+			if host.Name == name {
+				fmt.Printf("Host already exists: %s\n", name)
+				os.Exit(1)
+			}
+		}
+
+		hosts = append(hosts, host{name})
 	}
 
-	if hostsRemove != "" {
+	if hostsRemove {
 		var newHosts []host
+		var removed bool
 
 		for _, host := range hosts {
-			if host.Name != hostsRemove {
+			if host.Name != name {
 				newHosts = append(newHosts, host)
+			} else {
+				removed = true
 			}
+		}
+
+		if !removed {
+			fmt.Printf("Host not found: %s\n", name)
+			os.Exit(1)
 		}
 
 		hosts = newHosts
--- src/main.go
+++ src/main.go
@@ -54,7 +54,7 @@ func main() {
 
 		expandWorkDirPath()
 
-		cmd.run(cmdArgs)
+		cmd.run(flagset.Args())
 	}
 
 	/*
