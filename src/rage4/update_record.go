f2a95b7f2ca6d247b31d28bd71d89e02e29ae6a9
Richard Collins <richard@tawherotech.nz>
2016-06-27 10:44:42 +1200

    Remove debugging code from rage4 bindings

--- src/rage4/update_record.go
+++ src/rage4/update_record.go
@@ -1,58 +1,53 @@
 package rage4
 
 import (
-  "fmt"
-  "strconv"
+	"fmt"
+	"strconv"
 )
 
 // NOTE: NOT YET WORKING!
 
-func (c *Client) UpdateRecord( recordId int, record Record) (status Status, err error) {
+func (c *Client) UpdateRecord(recordId int, record Record) (status Status, err error) {
 
-  // create http request
-  endpoint := fmt.Sprintf("updaterecord/%d", recordId)
-  idSetting := strconv.Itoa(record.Id)
-  activeSetting := strconv.FormatBool(record.IsActive)
-  prioritySetting := strconv.Itoa(record.Priority)
-  ttlSetting := strconv.Itoa(record.TTL)
-  geozoneSetting := strconv.Itoa(record.GeoRegionId)
-  
-  parameters := map[string]string {
-    "id" : idSetting,
-    "name" : record.Name,
-    "content" : record.Content,
-    "priority" : prioritySetting,
-    "active" : activeSetting,
-    "ttl" : ttlSetting,
-    "geozone" : geozoneSetting,
-  }
+	// create http request
+	endpoint := fmt.Sprintf("updaterecord/%d", recordId)
+	idSetting := strconv.Itoa(record.Id)
+	activeSetting := strconv.FormatBool(record.IsActive)
+	prioritySetting := strconv.Itoa(record.Priority)
+	ttlSetting := strconv.Itoa(record.TTL)
+	geozoneSetting := strconv.Itoa(record.GeoRegionId)
 
-  fmt.Printf("%s\n",endpoint)
-  req, err := c.NewRequest(nil, "GET", endpoint, parameters)
-  if err != nil {
-    return Status{}, err
-  }
+	parameters := map[string]string{
+		"id":       idSetting,
+		"name":     record.Name,
+		"content":  record.Content,
+		"priority": prioritySetting,
+		"active":   activeSetting,
+		"ttl":      ttlSetting,
+		"geozone":  geozoneSetting,
+	}
 
-  // issue the API request
-  resp, err := c.Http.Do(req)
-  if err != nil {
-    return Status{}, err
-  }
-  defer resp.Body.Close()
+	//fmt.Printf("%s\n",endpoint)
+	req, err := c.NewRequest(nil, "GET", endpoint, parameters)
+	if err != nil {
+		return Status{}, err
+	}
 
-  // parse the response
-  getStatusResponse := Status{}
-  err = decode(resp.Body, &getStatusResponse)
-  if err != nil {
-    return Status{}, err
-  }
+	// issue the API request
+	resp, err := c.Http.Do(req)
+	if err != nil {
+		return Status{}, err
+	}
+	defer resp.Body.Close()
 
-  status = getStatusResponse
-  
-  return status, nil
+	// parse the response
+	getStatusResponse := Status{}
+	err = decode(resp.Body, &getStatusResponse)
+	if err != nil {
+		return Status{}, err
+	}
+
+	status = getStatusResponse
+
+	return status, nil
 }
-
-
-
-
-
