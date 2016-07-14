38818f1235a4a65668790991e491f0576a28d923
Richard Collins <richard@tawherotech.nz>
2016-07-14 12:24:44 +1200

    Make sure zerotier comes up as zt0 after reconfigure

--- src/hosts.go
+++ src/hosts.go
@@ -346,6 +346,16 @@ const configureHostScript = `
 set -v -e
 
 
+# Leave zerotier networks before joining so interface comes up as zt0
+zerotier-cli listnetworks | cut -d ' ' -f 3 | while read networkId
+do
+   if [ "$networkId" != "<nwid>" ]
+   then
+      zerotier-cli leave $networkId
+   fi
+done
+
+# Join zerotier network
 if [ -n "{{.ZerotierNetworkId}}" ]
 then
 	zerotier-cli join {{.ZerotierNetworkId}}
